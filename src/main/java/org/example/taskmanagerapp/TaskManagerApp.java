d0d5a076951c43ba6a6b2eefa4e385360f44707b
Aiman Hakimi <aiman17hakimi@gmail.com>
2025-05-19 23:48:07 +0800

    made some experimental changes regarding ui design

--- src/main/java/org/example/taskmanagerapp/TaskManagerApp.java
+++ src/main/java/org/example/taskmanagerapp/TaskManagerApp.java
@@ -1,10 +1,19 @@
 package org.example.taskmanagerapp;
 
-import java.time.LocalDate;
-
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,24 +23,91 @@ public class TaskManagerApp extends Application{
     @Override
     public void start(Stage stage) throws IOException {
 
-        VBox root = new VBox();
-        Scene scene = new Scene(root, 320, 240);
-        stage.setTitle("Hello!");
+        Image nexusLogo = new Image("file:src/main/java/org/example/taskmanagerapp/Nexus.png");
+        ImageView imageView = new ImageView(nexusLogo);
+        imageView.setFitHeight(48);
+        imageView.setPreserveRatio(true);
+
+        Border inputBorder = new Border(new BorderStroke(
+                Color.BLACK,
+                BorderStrokeStyle.SOLID,
+                new CornerRadii(50),
+                new BorderWidths(0)
+        ));
+
+        // Task Name
+        Label taskNameLabel = new Label("Task Name");
+        taskNameLabel.setPrefHeight(25);
+        taskNameLabel.setTextFill(Color.WHITE);
+
+        TextField taskNameTField = new TextField();
+
+        // Task Category
+        Label taskCategoryLabel = new Label("Category");
+        taskCategoryLabel.setPrefHeight(25);
+        taskCategoryLabel.setTextFill(Color.WHITE);
+
+        TextField taskCategoryTField = new TextField();
+
+        // Task Due Date
+        Label taskDueLabel = new Label("Due Date");
+        taskDueLabel.setPrefHeight(25);
+        taskDueLabel.setTextFill(Color.WHITE);
+
+        TextField taskDueTField = new TextField();
+
+        // Task Priority
+        Label taskPriorityLabel = new Label("Priority");
+        taskPriorityLabel.setPrefHeight(25);
+        taskPriorityLabel.setTextFill(Color.WHITE);
+
+        TextField taskPriorityTField = new TextField();
+
+        VBox taskName = new VBox(taskNameLabel, taskNameTField);
+        VBox taskCategory = new VBox(taskCategoryLabel, taskCategoryTField);
+        VBox taskDueDate = new VBox(taskDueLabel, taskDueTField);
+        VBox taskPriority = new VBox(taskPriorityLabel, taskPriorityTField);
+
+        VBox inputs = new VBox(10, taskName, taskCategory, taskDueDate, taskPriority);
+
+
+        // Add Task Button
+        Button addTaskButton = new Button("ADD");
+        addTaskButton.setPrefWidth(Double.MAX_VALUE);
+        addTaskButton.setTextFill(Color.WHITE);
+        addTaskButton.setFont(Font.font("Arial", FontWeight.NORMAL, FontPosture.REGULAR, 13));
+        addTaskButton.setBackground(Background.fill(Color.web("0F7D12")));
+
+        VBox inputSection = new VBox(50, inputs, addTaskButton);
+        inputSection.setPrefWidth(227);
+
+
+        Label outputLabel = new Label("Your To-Do List");
+        outputLabel.setTextFill(Color.WHITE);
+        outputLabel.setFont(Font.font("Arial", FontWeight.NORMAL, FontPosture.REGULAR, 16));
+
+        VBox displaySection = new VBox(10, outputLabel);
+        displaySection.setPadding(new Insets(22, 10, 22, 10));
+
+
+        HBox titleSection = new HBox(imageView);
+        titleSection.setAlignment(Pos.CENTER);
+
+        HBox contentSection = new HBox(74, inputSection, displaySection);
+        contentSection.setPadding(new Insets(10));
+
+        VBox root = new VBox(10, titleSection, contentSection);
+        root.setBackground(Background.EMPTY);
+        root.setPadding(new Insets(20, 10, 20, 10));
+
+        Scene scene = new Scene(root, 770, 500);
+        scene.setFill(Color.BLACK);
+        stage.setTitle("Task Manager App");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-
-        Task task1 = new Task("Vacuum Floor", "Cleaning", LocalDate.of(2025, 10, 24) , "High");
-        Task task2 = new Task("Watch Netflix", "Entertainment", LocalDate.of(2025, 5, 24), "Urgent");
-
-        System.out.println(task1);
-        System.out.println(TaskValidator.validateTask(task1));
-        System.out.println();
-        System.out.println(task2);
-        System.out.println(TaskValidator.validateTask(task2));
-
         launch();
     }
 
